c1d8a6cfe6fcc887af174ef0245365c19c8b3030
Ray Andrew <raydreww@gmail.com>
2020-05-03 21:47:22 +0700

    [WIP] revert back to 150 tasks

--- sources/openjdk8/hotspot/src/share/vm/gc_implementation/parallelScavenge/ucare.psgc.inline.hpp
+++ sources/openjdk8/hotspot/src/share/vm/gc_implementation/parallelScavenge/ucare.psgc.inline.hpp
@@ -127,7 +127,7 @@ private:
     uint            _last_idx;
 
 public:
-    static GCWorkerTracker* create(uint id, uint max_gc_worker_tasks = 1000) {
+    static GCWorkerTracker* create(uint id, uint max_gc_worker_tasks = 150) {
       return new GCWorkerTracker(id, max_gc_worker_tasks);
     }
 
